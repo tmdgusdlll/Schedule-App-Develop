e6ac862f92bfa325ae7cbfb2450f1e0b54470f28
tmdgsudl <leesh27955@gmail.com>
2026-02-10 19:48:28 +0900

    init project

---
+++ src/main/java/com/scheduleappdevelop/ScheduleAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleappdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleAppDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scheduleappdevelop/ScheduleAppDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleappdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
