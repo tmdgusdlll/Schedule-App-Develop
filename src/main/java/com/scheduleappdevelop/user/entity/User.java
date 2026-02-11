5c1d03a436ba8da49806e080aa4d12204e48dd97
tmdgsudl <leesh27955@gmail.com>
2026-02-11 11:39:11 +0900

    [Lv.3] 회원가입 - 비밀번호 필드(8글자 이상) 추가 완료

--- src/main/java/com/scheduleappdevelop/user/entity/User.java
+++ src/main/java/com/scheduleappdevelop/user/entity/User.java
@@ -3,6 +3,8 @@ package com.scheduleappdevelop.user.entity;
 import com.scheduleappdevelop.schedule.entity.BaseEntity;
 import com.scheduleappdevelop.schedule.entity.Schedule;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,10 +22,14 @@ public class User extends BaseEntity {
     private Long id;
     private String name;
     private String email;
+    // Lv.3 회원가입 (비밀번호 추가) / 8글자 이상
+    @Size(min = 8, max = 16)
+    private String password;
 
-    public User(String name, String email) {
+    public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
+        this.password = password;
     }
 
     public void update(String name) {
